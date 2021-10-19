b0fd63680d6dc9ed180a2a74b0be67f311c7151e
DoctorRidley <bckoshy97@gmail.com>
2021-10-19 01:04:00 -0700

    Added method to close DB connection, resolved some errors

--- src/main/scala/repos/CRUDRepo.scala
+++ src/main/scala/repos/CRUDRepo.scala
@@ -1,15 +1,14 @@
 package repos
 
 import models.Iris
-
 import org.mongodb.scala._
+import org.mongodb.scala.model.Filters.{and, equal}
 
 object CRUDRepo {
 
     val client: MongoClient = MongoClient()
     val db: MongoDatabase = client.getDatabase("Iris")
     val iris: MongoCollection[Document] = db.getCollection("iris")
-
 
     def Create_Iris(i: Iris): Unit = {
 
@@ -24,15 +23,29 @@ object CRUDRepo {
         iris.insertOne(doc)
     }
 
-    def Read_Iris(i: Iris): Unit = {
+    def Read_Iris(i: Iris): Iris = {
 
+        // FIX ME: may not need all properties
+        val record: FindObservable[Document] = iris.find(and(
+            equal("sepal_width", i.Sepal_Width()),
+            equal("sepal_length", i.Sepal_Length()),
+            equal("petal_width", i.Petal_Width()),
+            equal("petal_length", i.Petal_Length()),
+            equal("species", i.Species())
+        ))
+
+        //val observable: Observable[Completed]
+
+        return null
     }
 
     def Update_Iris(i: Iris): Unit = {
-
+        val old_iris: Iris = Read_Iris(i)
     }
 
     def Delete_Iris(i: Iris): Unit = {
 
     }
+
+    def Close(): Unit = client.close()
 }
